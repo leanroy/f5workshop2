6c7029dac446326597d64f9c0f7b95ecb53026cc
Leandro Burton <leandroroyb@gmail.com>
2021-01-11 23:15:31 +0000

    Added changes in dns1_r.py

--- dns1_r.py
+++ dns1_r.py
@@ -1,31 +1,42 @@
 from dns.resolver import Resolver
+import csv
 
 my1_r = Resolver()
 my1_r.nameservers = ['8.8.8.8']
-
-names = ['secure.worldpay.com','cisco.com']
-print("Using server: ",my1_r.nameservers)
-
-for name in names:
-    print("-")
-    for qtype in 'A', 'CNAME':
-        answer1 = my1_r.query(name,qtype,raise_on_no_answer=False)
-        if answer1.rrset is not None:
-            print(answer1.rrset)
-
-
 my2_r = Resolver()
 my2_r.nameservers = ['8.8.4.4']
+
+# Create empty dictionaries
+names = {}
+resolved1 = {}
+resolved2 = {}
+
+# Open toresolve.csv file and populate dictionary names
+with open('toresolve.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    names = dict((rows[0],0) for rows in reader)
+    infile.close()
+
+print("Using server: ",my1_r.nameservers)
+
+for x in names:
+   try:
+      hostip = my1_r.resolve(x,raise_on_no_answer=False)
+   except:
+      hostip = 'None'
+   resolved1[x] = str(hostip.rrset[0])
+
+print(resolved1)
 
 print("""-------
 -------
 Using server: """,my2_r.nameservers)
 
-for name in names:
-    print("-")
-    for qtype in 'A', 'CNAME':
-        answer2 = my2_r.query(name,qtype,raise_on_no_answer=False)
-        if answer2.rrset is not None:
-            print(answer2.rrset)
+for x in names:
+   try:
+      hostip = my2_r.resolve(x,raise_on_no_answer=False)
+   except:
+      hostip = 'None'
+   resolved2[x] = str(hostip.rrset[0])
 
-
+print(resolved2)
